547ab2f2de83619ef530cfc76b942c0e70308726
fullstack251022 <116830126+fullstack251022@users.noreply.github.com>
2022-10-28 12:56:55 +0300

    Create ConditionsAndPlusPlus.java

---
+++ ConditionsAndPlusPlus.java
@@ -0,0 +1,74 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Start");
+
+
+        int x = 5;
+//        x = x+1; //x++;
+//        ++x;
+//        x--;
+
+        //the difference between ++x and x++;
+//        int y = 10 + x++;
+//        int y = 10 + ++x;
+//        System.out.println("y:" + y);
+//        System.out.println("x: " + x);
+
+
+//          x = x + 20;
+//          x += 20; //same as x = x + 20;
+//        x -= 20;  //same as x = x - 20;
+//        x /= 20; // same as x = x / 20;
+//        x *= 20; // same as x = x * 20;
+//        System.out.println(x);
+
+
+//        int age = 18;
+//
+//        if(age>18){
+//            System.out.println("You are older than 18");
+//        }else if(age<18){
+//            System.out.println("You are younger than 18");
+//        }else {
+//            System.out.println("You are equal to 18 exactly");
+//        }
+
+
+//        if(age>=18){
+//            System.out.println("You are older or equal than 18");
+//        }else {
+//            System.out.println("you are younger than 18");
+//        }
+
+
+//        int number = 30;
+//
+//        if (number >= 18) {
+//            System.out.println("Higher or equal 18");
+//        }
+//
+//        if (number < 18) {
+//            System.out.println("Number Lesser then 18");
+//        }
+//
+//        if (number!=30){
+//            System.out.println("Number is not 30");
+//        }
+
+//        if (number > 18) {
+//            System.out.println("The number is higher than 18");
+//        }
+//        if (number == 18){
+//            System.out.println("The number is 18");
+//        }
+//        if (number < 18){
+//            System.out.println("The number is lower than 18");
+//        }
+
+
+        System.out.println("Process ended");
+
+
+    }
+}
