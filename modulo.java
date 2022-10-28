4e6a9ea33a90e99eb49fc7e733e36e19ee253fb5
fullstack251022 <116830126+fullstack251022@users.noreply.github.com>
2022-10-28 12:59:21 +0300

    Create modulo.java

---
+++ modulo.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Beni");
+        int x, y, z;
+
+        x = 8;
+        y = 20;
+        z = y % x;
+        
+
+//        System.out.println(x);
+//        System.out.println(y);
+        System.out.println(z);
+
+    }
+}
+
