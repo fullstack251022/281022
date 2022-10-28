6552b1b037df477439e82f4ae463cdeb8a2fb706
fullstack251022 <116830126+fullstack251022@users.noreply.github.com>
2022-10-28 10:47:50 +0300

    Create ScannerAndTypes.java

---
+++ ScannerAndTypes.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+
+        //kumkum
+        //Kumkum k = new Kumkum("Red","Tefal");
+        //k.makeTea()
+        //k.makeCodee
+
+
+
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("Please enter your name");
+        String myString = s.nextLine();
+        System.out.println("Please enter your age ");
+        String age = s.nextLine();
+        System.out.println("Please enter city you live in");
+        String city = s.nextLine();
+
+
+        System.out.println("Your name is "+myString+" and your age is "+age+" and you are living in "+city);
+
+
+        System.out.println("Hello world");
+
+
+//        String myString = "bla bla bla";
+//
+//        int age = 34;
+//
+//        byte myByte = 5; //-128 - 127;
+//        short myShort = 32767;
+//        int myInt = 4000;
+//        long myLong = 70000;
+//
+//        float myFloat = 45.23f;
+//        double myDouble = 23.3002384092834092840923489;
+//
+//        char myChar = 'A';
+//
+//        boolean myBoolean = false; //true false
+//        boolean isTrue = 1 > 3;
+//        System.out.println(isTrue);
+//        System.out.println(myString);
+
+//        String firstName = "Beni";
+//        String fullSentence = "My name is " + firstName + " and my age is " + age;
+//
+//        System.out.println(fullSentence);
+    }
+}
